54bf5f042ad37e0b760df26a02fdec80cddc7308
mc-mathco <118497949+mc-mathco@users.noreply.github.com>
2022-11-18 18:33:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 import numpy as np
 import pandas as pd
 import re
+import nltk
+nltk.download('punkt')
 from nltk.tokenize import sent_tokenize
 from sentence_transformers import SentenceTransformer
 from umap import UMAP
